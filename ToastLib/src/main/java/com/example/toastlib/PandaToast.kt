9d8d2beef8fcbab86367d76b713b954ac3f6cc47
Saad Ali Shujaat <saadali0075368@gamil.com>
2021-10-24 23:12:40 -0700

    object

--- ToastLib/src/main/java/com/example/toastlib/PandaToast.kt
+++ ToastLib/src/main/java/com/example/toastlib/PandaToast.kt
@@ -3,9 +3,10 @@ package com.example.toastlib
 import android.content.Context
 import android.widget.Toast
 
-object PandaToast {
-
-    fun show(context: Context){
-        Toast.makeText(context , " hello ", Toast.LENGTH_LONG).show()
+class PandaToast {
+    companion object {
+        fun show(context: Context) {
+            Toast.makeText(context, " hello ", Toast.LENGTH_LONG).show()
+        }
     }
 }
